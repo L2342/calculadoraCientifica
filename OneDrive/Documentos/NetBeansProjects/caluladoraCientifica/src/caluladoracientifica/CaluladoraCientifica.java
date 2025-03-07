761a9c02038d98d5bf1a216e0d01fa49d8a41ef3
Berserk-24 <mitgard04@gmail.com>
2025-03-07 16:54:06 -0500

    Merge origin/main into main

--- OneDrive/Documentos/NetBeansProjects/caluladoraCientifica/src/caluladoracientifica/CaluladoraCientifica.java
+++ OneDrive/Documentos/NetBeansProjects/caluladoraCientifica/src/caluladoracientifica/CaluladoraCientifica.java
@@ -1,3 +1,4 @@
+//hola
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -15,5 +16,4 @@ public class CaluladoraCientifica {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    //hola4
 }
