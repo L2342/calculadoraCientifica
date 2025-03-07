02d253d3679c6d32284f1cf2057079ac207c3364
Berserk-24 <mitgard04@gmail.com>
2025-03-07 17:02:12 -0500

    Merge origin/main into main

---
+++ OneDrive/Documentos/NetBeansProjects/caluladoraCientifica/src/caluladoracientifica/pureba.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caluladoracientifica;
+
+/**
+ *
+ * @author Juan_
+ */
+public class pureba {
+    //hola
+}
