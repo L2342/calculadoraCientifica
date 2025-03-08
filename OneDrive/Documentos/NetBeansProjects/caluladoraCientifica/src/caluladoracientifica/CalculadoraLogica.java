2a8257e561c47e99628c734363aebf1e2be33b1c
Berserk-24 <mitgard04@gmail.com>
2025-03-08 18:16:38 -0500

    clase Logica, ln

---
+++ OneDrive/Documentos/NetBeansProjects/caluladoraCientifica/src/caluladoracientifica/CalculadoraLogica.java
@@ -0,0 +1,11 @@
+package caluladoracientifica;
+
+public class CalculadoraLogica {
+
+        // TODO code application logic here
+        
+        public static double calcularLn(double x) {
+            
+            return Math.log(x);
+        }
+}
